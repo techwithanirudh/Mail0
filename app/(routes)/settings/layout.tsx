ea904a2a25bbfa543c46a5a1969e02c8c6674a8f
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-02-18 23:12:06 +0530

    Update app/(routes)/settings/layout.tsx
    Co-authored-by: Praash <99237795+Praashh@users.noreply.github.com>

--- app/(routes)/settings/layout.tsx
+++ app/(routes)/settings/layout.tsx
@@ -2,8 +2,6 @@
 
 import { SidebarToggle } from "@/components/ui/sidebar-toggle";
 import { ScrollArea } from "@/components/ui/scroll-area";
-// import { navigationConfig } from "@/config/navigation";
-// import { usePathname } from "next/navigation";
 import { Suspense } from "react";
 
 export default function SettingsLayout({ children }: { children: React.ReactNode }) {
