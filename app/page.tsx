a3f4999cc792263dc5f65d39e8eb2a6935724a72
Anirudh Sriram <hello@techwithanirudh.com>
2025-02-20 04:57:29 +0000

    style: use legacy

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Spotlight } from "@/components/ui/spotlight";
 import HeroImage from "@/components/home/hero-image";
 import { useSession } from "@/lib/auth-client";
@@ -8,11 +9,13 @@ import Hero from "@/components/home/hero";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (session) {
-    redirect("/mail/inbox");
-  }
+  // if (session) {
+  //   redirect("/mail/inbox");
+  // }
+
   return (
-    <div className="relative h-screen min-h-screen w-full overflow-y-auto bg-grid-small-black/[0.039] dark:bg-grid-small-white/[0.025]">
+    <div className="relative h-screen min-h-screen w-full overflow-y-auto scroll-smooth bg-grid-small-black/[0.039] dark:bg-grid-small-white/[0.025]">
+      {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
         <div className="h-[900px] w-[900px] rounded-full bg-gradient-to-r from-white/5 via-white/5 to-white/5 blur-[200px]" />
       </div>
