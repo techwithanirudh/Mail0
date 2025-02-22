df5581f289e1d8add0e430b8ef75e93fc076b366
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-02-22 19:20:12 +0000

    fix

--- components/ui/lanyard/index.tsx
+++ components/ui/lanyard/index.tsx
@@ -1,14 +1,18 @@
-import Spline from "@splinetool/react-spline/next";
+"use client";
+import Spline from "@splinetool/react-spline";
+import { Suspense } from "react";
 
 export default function Lanyard() {
   return (
-    <Spline
-      scene="https://prod.spline.design/iYYVHpYLyGIvh5sX/scene.splinecode"
-      style={{ width: "150%", height: "160%" }}
-      className="absolute -left-1/4 -top-1/4 scale-[.5] md:scale-[.8]"
-      onSplineMouseDown={() => {
-        console.log("thanku");
-      }}
-    />
+    <Suspense fallback={<p>Loadin</p>}>
+      <Spline
+        scene="https://prod.spline.design/iYYVHpYLyGIvh5sX/scene.splinecode"
+        style={{ width: "150%", height: "160%" }}
+        className="absolute -left-1/4 -top-1/4 scale-[.5] md:scale-[.8]"
+        onSplineMouseDown={() => {
+          alert("Thank yall");
+        }}
+      />
+    </Suspense>
   );
 }
