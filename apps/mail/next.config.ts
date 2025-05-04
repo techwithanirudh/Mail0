451a337e0f231dbaeb2d57aa330f19fb12430227
Aditya Tripathi <aditya@climactic.co>
2025-05-04 18:33:18 +0000

    feat: improve docker image, compose file, add migrations image, etc.

--- apps/mail/next.config.ts
+++ apps/mail/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // devIndicators: false,
+  output: process.env.DOCKER_BUILD ? 'standalone' : undefined,
   compiler: {
     removeConsole:
       process.env.NODE_ENV === 'production'
