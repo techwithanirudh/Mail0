f9b6f49e3a5ffe91a7954dd7f1bda3f9eab7c92d
Nizzy <nizabizaher@gmail.com>
2025-05-04 00:31:33 -0700

    homepage

--- apps/mail/components/home/HomeContent.tsx
+++ apps/mail/components/home/HomeContent.tsx
@@ -10,9 +10,21 @@ import {
   ListItem,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
+import {
+  ArrowRight,
+  ChevronDown,
+  CurvedArrow,
+  Discord,
+  GitHub,
+  LinkedIn,
+  Twitter,
+  Plus,
+  Cube,
+  MediumStack,
+  Clock,
+} from '../icons/icons';
 import { PixelatedBackground, PixelatedLeft, PixelatedRight } from '@/components/home/pixelated-bg';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
-import { ArrowRight, CurvedArrow, Discord, GitHub, LinkedIn, Twitter } from '../icons/icons';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { use, useCallback, useEffect, useRef, useState } from 'react';
@@ -326,7 +338,6 @@ export default function HomeContent() {
           width={1920}
           height={1080}
           className="relative opacity-70"
-          
         />
         <div className="relative bottom-48">
           <div className="flex items-center justify-center">
@@ -339,7 +350,146 @@ export default function HomeContent() {
             <h1 className="text-6xl font-medium text-white/40">Reply in seconds</h1>
           </div>
           <div className="relative bottom-3 flex items-center justify-center">
-            <Image src="/compose.png" alt="speed" width={550} height={800} />
+          <div className="mx-auto inline-flex max-w-[600px] flex-col items-center justify-center overflow-hidden rounded-2xl  bg-[#1A1A1A] shadow-md">
+        <div className="inline-flex h-12 items-center justify-start gap-2 self-stretch border-b-[0.50px] p-4">
+          <div className="text-base-gray-500/50 justify-start text-sm leading-none">To:</div>
+          <div className="flex flex-1 items-center justify-start gap-1">
+            <div className="outline-tokens-badge-default/10 flex items-center justify-start gap-1.5 rounded-full py-1 pl-1 pr-1.5 border border-[#2B2B2B]">
+              <Image
+                height={20}
+                width={20}
+                className="h-5 w-5 rounded-full"
+                src="/adam.jpg"
+                alt="Adam"
+              />
+              <div className="flex items-center justify-start">
+                <div className="flex items-center justify-center gap-2.5 pr-0.5">
+                  <div className="text-base-gray-950 justify-start text-sm leading-none">Adam</div>
+                </div>
+              </div>
+            </div>
+            <div className="outline-tokens-badge-default/10 flex items-center justify-start gap-1.5 rounded-full py-1 pl-1 pr-1.5 border border-[#2B2B2B]">
+            <Image
+                height={20}
+                width={20}
+                className="h-5 w-5 rounded-full"
+                src="/ryan.jpg"
+                alt="Ryan"
+              />              <div className="flex items-center justify-start">
+                <div className="flex items-center justify-center gap-2.5 pr-0.5">
+                  <div className="text-base-gray-950 justify-start text-sm leading-none">Ryan</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="inline-flex h-12 items-center justify-start gap-2.5 self-stretch p-4">
+          <Clock className="relative h-3.5 w-3.5 overflow-hidden fill-[#9A9A9A]" />
+          <div className="inline-flex flex-1 flex-col items-start justify-start gap-3">
+            <div className="inline-flex items-center justify-start gap-1 self-stretch">
+              <div className="text-base-gray-950 flex-1 justify-start text-sm font-normal leading-none">
+                Re: Code review feedback
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-start gap-12 self-stretch rounded-2xl bg-[#202020] px-4 py-3">
+          <div className="flex flex-col items-start justify-start gap-3 self-stretch">
+            <div className="text-white justify-start self-stretch text-sm font-normal leading-normal">
+              Hey team,
+            </div>
+            <div className="text-white justify-start self-stretch text-sm font-normal leading-normal">
+              I took a look at the code review feedback. Really like the keyboard navigation - it
+              makes everything much faster to access. The search implementation is clean, though I'd
+              love to see the link to test it out myself.
+            </div>
+            <div className="text-white justify-start self-stretch text-sm font-normal leading-normal">
+              Let me know when you can share the preview and I'll provide more detailed feedback.
+            </div>
+          </div>
+          <div className="inline-flex items-center justify-between self-stretch">
+            <div className="flex items-center justify-start gap-3">
+              <div className="flex items-center justify-start rounded-md bg-white text-black">
+                <div className="flex h-7 items-center justify-center gap-1.5 overflow-hidden rounded-bl-md rounded-tl-md bg-white pl-1.5 pr-1">
+                  <div className="flex items-center justify-center gap-2.5 pl-0.5">
+                    <div className="text-black justify-start text-center text-sm leading-none">
+                      Send now
+                    </div>
+                  </div>
+                  <div className="flex h-5 items-center justify-center gap-2.5 rounded bg-[#E7E7E7] px-1 outline outline-1 outline-offset-[-1px] outline-[#D2D2D2]">
+                    <div className="text-tokens-shortcut-primary-symbol justify-start text-center text-sm font-semibold leading-none">
+                      ⏎
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-base-gray-950 flex items-center justify-start gap-2.5 self-stretch px-2 pr-3">
+                  <div className="relative h-3 w-px rounded-full bg-[#D0D0D0]" />
+                </div>
+                <div className="bg-base-gray-950 flex h-7 items-center justify-center gap-1.5 overflow-hidden rounded-br-md rounded-tr-md pr-2">
+                  <ChevronDown className="relative h-2 w-2 overflow-hidden fill-black" />
+                </div>
+              </div>
+              <div className="flex h-7 items-center justify-center gap-0.5 overflow-hidden rounded-md bg-[#373737] px-1.5">
+                <Plus className="relative h-2.5 w-2.5 overflow-hidden fill-[#9A9A9A]" />
+                <div className="flex items-center justify-center gap-2.5 px-0.5">
+                  <div className="text-base-gray-950 justify-start text-sm leading-none">
+                    Add files
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-3">
+              <div className="flex h-7 items-center justify-center gap-0.5 overflow-hidden rounded-md bg-[#373737] px-1.5">
+                <Cube className="relative h-3 w-3 overflow-hidden fill-[#9A9A9A]" />
+
+                <div className="flex items-center justify-center gap-2.5 px-0.5">
+                  <div className="text-base-gray-950 justify-start text-sm leading-none">
+                    Neutral
+                  </div>
+                </div>
+              </div>
+              <div className="flex h-7 items-center justify-center gap-0.5 overflow-hidden rounded-md bg-[#373737] px-1.5">
+                <MediumStack className="relative h-2.5 w-2.5 overflow-hidden fill-[#9A9A9A] mx-1" />
+
+                <div className="flex items-center justify-center gap-2.5 px-0.5">
+                  <div className="text-base-gray-950 justify-start text-sm leading-none">
+                    Medium-length
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="inline-flex items-start justify-start self-stretch">
+          <div className="border-tokens-stroke-light/5 flex h-12 flex-1 items-center justify-center gap-2 border-r-[0.50px]">
+            <div className="flex items-center justify-start gap-1">
+              <div className="bg-[#2B2B2B] flex h-5 w-5 items-center justify-center gap-2.5 rounded-[5px] px-1.5">
+                <div className="text-[#8C8C8C] justify-start text-center text-sm font-semibold leading-none">
+                  ↓
+                </div>
+              </div>
+              <div className="bg-[#2B2B2B] flex h-5 w-5 items-center justify-center gap-2.5 rounded-[5px] px-1.5">
+                <div className="text-[#8C8C8C] justify-start text-center  text-sm font-semibold leading-none">
+                  ↑
+                </div>
+              </div>
+            </div>
+            <div className="text-[#8C8C8C] justify-start text-sm leading-none">
+              to navigate
+            </div>
+          </div>
+          <div className="flex h-12 flex-1 items-center justify-center gap-2">
+            <div className="bg-[#2B2B2B] flex h-5 items-center justify-center gap-2.5 rounded-[5px] px-1">
+              <div className="text-[#8C8C8C] justify-start text-center text-sm font-semibold leading-none">
+                ⌘Z
+              </div>
+            </div>
+            <div className="text-[#8C8C8C] justify-start text-sm leading-none">
+              return generation
+            </div>
+          </div>
+        </div>
+      </div>
           </div>
         </div>
       </div>
@@ -347,32 +497,44 @@ export default function HomeContent() {
       <div className="relative bottom-96 bg-[#111111]">
         <div className="items-center justify-center gap-8 md:flex">
           <div className="flex flex-col">
-            <Image src="/mail-list.png" alt="speed" width={400} height={800} className='mb-4'/>
-            <div className='gap-4'>
-              <h1 className='text-white text-xl font-medium leading-loose mb-2'>Lightning-Fast Interface</h1>
-              <p className='max-w-sm font-light text-white text-sm'>Email at the speed of thought. Navigate your entire inbox using just your keyboard. Process hundreds of emails in minutes.</p>
+            <Image src="/mail-list.png" alt="speed" width={400} height={800} className="mb-4" />
+            <div className="gap-4">
+              <h1 className="mb-2 text-xl font-medium leading-loose text-white">
+                Lightning-Fast Interface
+              </h1>
+              <p className="max-w-sm text-sm font-light text-white">
+                Email at the speed of thought. Navigate your entire inbox using just your keyboard.
+                Process hundreds of emails in minutes.
+              </p>
             </div>
           </div>
           <div>
-            <Image src="/ai-summary.png" alt="speed" width={400} height={800} className='mb-4'/>
+            <Image src="/ai-summary.png" alt="speed" width={400} height={800} className="mb-4" />
             <div>
-              <h1 className='text-white text-lg font-medium leading-loose mb-2'>AI-Powered Summaries</h1>
-              <p className='max-w-sm font-light text-white text-sm'>Your personal email copilot. Let our AI draft responses, summarize long threads, and extract action items automatically.</p>
+              <h1 className="mb-2 text-lg font-medium leading-loose text-white">
+                AI-Powered Summaries
+              </h1>
+              <p className="max-w-sm text-sm font-light text-white">
+                Your personal email copilot. Let our AI draft responses, summarize long threads, and
+                extract action items automatically.
+              </p>
             </div>
           </div>
-          <div className=''>
-            <Image src="/search.png" alt="speed" width={400} height={800} className='mb-4'/>
-            <div className=''>
-              <h1 className='text-white text-lg font-medium leading-loose mb-2'>Smart Search</h1>
-              <p className='max-w-sm font-light text-white text-sm'>Your inbox, your rules. Create personalized email processing flows that match exactly how you organize,write, reply, and work.</p>
+          <div className="">
+            <Image src="/search.png" alt="speed" width={400} height={800} className="mb-4" />
+            <div className="">
+              <h1 className="mb-2 text-lg font-medium leading-loose text-white">Smart Search</h1>
+              <p className="max-w-sm text-sm font-light text-white">
+                Your inbox, your rules. Create personalized email processing flows that match
+                exactly how you organize,write, reply, and work.
+              </p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="relative bottom-70 z-[-2]">
-       
-        <div className="relative bottom-48 z-1">
+      <div className="bottom-70 relative z-[-2]">
+        <div className="z-1 relative bottom-48">
           <div className="flex items-center justify-center">
             <h1 className="text-xl font-light text-white/40">
               AI email chat with natural language
@@ -380,21 +542,61 @@ export default function HomeContent() {
           </div>
           <div className="mt-8 flex flex-col items-center justify-center">
             <h1 className="text-6xl font-medium text-white">Ask away</h1>
-            <h1 className="text-6xl font-medium  text-white/40">Get your answers</h1>
+            <h1 className="text-6xl font-medium text-white/40">Get your answers</h1>
           </div>
           <div className="relative bottom-2 flex items-center justify-center">
             <Image src="/ai-chat.png" alt="speed" width={850} height={800} />
           </div>
           <Image
-          src="/pixel.svg"
-          alt="hero"
-          width={1920}
-          height={1080}
-          className="relative opacity-70 rotate-180 bottom-16 z-[-2]"
-          style={{ clipPath: 'inset(45% 0 0 0)' }}
-        />
+            src="/pixel.svg"
+            alt="hero"
+            width={1920}
+            height={1080}
+            className="relative bottom-16 z-[-2] rotate-180 opacity-70"
+            style={{ clipPath: 'inset(45% 0 0 0)' }}
+          />
         </div>
       </div>
+
+      <div className="relative bottom-72 z-[-2]">
+        <div className="mx-auto max-w-[920px] text-center text-4xl font-normal leading-[48px] text-white">
+          <span className="text-[#B7B7B7]">Work smarter, not harder.</span>{' '}
+          <span className="pr-8 text-white">Automate repetitive</span>{' '}
+          <span className="text-[#B7B7B7]">email</span>
+          <span className="text-[#B7B7B7]"> tasks with</span>{' '}
+          <span className="pr-8 text-white">smart templates, </span>{' '}
+          <span className="text-white">scheduled sends</span>
+          <span className="text-[#B7B7B7]">
+            , follow-up reminders, and batch processing capabilities that
+          </span>{' '}
+          <br />
+          <span className="text-white underline">save hours every week.</span>
+        </div>
+        <div className="flex items-center justify-center">
+          <Image
+            className="relative bottom-12 right-[162px]"
+            src="/verified-home.png"
+            alt="tasks"
+            width={50}
+            height={50}
+          />
+          <Image
+            className="relative bottom-[150px] right-[-25px]"
+            src="/snooze-home.png"
+            alt="tasks"
+            width={50}
+            height={50}
+          />
+          <Image
+            className="relative bottom-[195px] left-[278px]"
+            src="/star-home.png"
+            alt="tasks"
+            width={50}
+            height={50}
+          />
+        </div>
+      </div>
+      
     </main>
   );
 }
