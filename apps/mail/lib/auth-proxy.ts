7605aeaa4b6fd9e542bce24de6850d4cc955b156
Aj Wazzan <x_1337@outlook.com>
2025-05-08 15:36:24 -0700

    save

--- apps/mail/lib/auth-proxy.ts
+++ apps/mail/lib/auth-proxy.ts
@@ -3,7 +3,9 @@ import { authClient } from './auth-client';
 export const authProxy = {
   api: {
     getSession: async ({ headers }: { headers: Headers }) => {
+      console.log('getSession', headers);
       const session = await authClient.getSession({ fetchOptions: { headers } });
+      console.log('session', session);
       if (session.error) throw new Error(session.error.message);
       return session.data;
     },
