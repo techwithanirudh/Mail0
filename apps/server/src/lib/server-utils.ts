d07e4f0f8e29093d8acc9304acfcb74beb69f430
Aj Wazzan <x_1337@outlook.com>
2025-05-09 22:23:36 -0700

    refactor: update connection handling and improve layout in NavMain component

--- apps/server/src/lib/server-utils.ts
+++ apps/server/src/lib/server-utils.ts
@@ -6,10 +6,13 @@ import { and, eq } from 'drizzle-orm';
 export const getActiveConnection = async (c: HonoContext) => {
   const { session, db } = c.var;
   if (!session?.user) throw new Error('Session Not Found');
-  if (!session.connectionId) throw new Error('No active connection found for the user');
+  if (!session.activeConnection?.id) throw new Error('No active connection found for the user');
 
   const activeConnection = await db.query.connection.findFirst({
-    where: and(eq(connection.userId, session.user.id), eq(connection.id, session.connectionId)),
+    where: and(
+      eq(connection.userId, session.user.id),
+      eq(connection.id, session.activeConnection.id),
+    ),
   });
 
   if (!activeConnection) throw new Error('Active connection not found');
@@ -25,18 +28,13 @@ export const connectionToDriver = (
   activeConnection: typeof connection.$inferSelect,
   c: HonoContext,
 ) => {
-  const driver = createDriver(
-    activeConnection.providerId,
-    {
-      auth: {
-        accessToken: activeConnection.accessToken,
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        refreshToken: activeConnection.refreshToken!,
-        email: activeConnection.email,
-      },
-      c,
+  const driver = createDriver(activeConnection.providerId, {
+    auth: {
+      accessToken: activeConnection.accessToken,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      refreshToken: activeConnection.refreshToken!,
+      email: activeConnection.email,
     },
-    c.env,
-  );
+  });
   return driver;
 };
