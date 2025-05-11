d0e5d4fa1bc2ab1cd2d9756e1c433cbdd6060a06
Aj Wazzan <x_1337@outlook.com>
2025-05-11 14:46:55 -0700

    feat: enhance NotificationProvider to handle message types and invalidate queries based on thread ID

--- apps/mail/components/party.tsx
+++ apps/mail/components/party.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useQueryClient } from '@tanstack/react-query';
 import { atom, useAtomValue, useSetAtom } from 'jotai';
 import { useTRPC } from '@/providers/query-provider';
 import { usePartySocket } from 'partysocket/react';
@@ -6,9 +7,9 @@ import { useSession } from '@/lib/auth-client';
 import { useEffect } from 'react';
 
 export const NotificationProvider = ({ headers }: { headers: Record<string, string> }) => {
-  //   const trpc = useTRPC();
+  const trpc = useTRPC();
   const { data: session } = useSession();
-
+  const queryClient = useQueryClient();
   usePartySocket({
     party: 'durable-mailbox',
     room: session?.activeConnection?.id ? `${session.activeConnection.id}` : 'general',
@@ -19,8 +20,15 @@ export const NotificationProvider = ({ headers }: { headers: Record<string, stri
       token: headers['cookie'],
     },
     host: process.env.NEXT_PUBLIC_BACKEND_URL!,
-    onMessage: (message) => {
-      console.log(message);
+    onMessage: (message: MessageEvent<string>) => {
+      const [threadId, type] = message.data.split(':');
+      if (type === 'end') {
+        console.log('invalidating thread', threadId);
+        queryClient.invalidateQueries({
+          queryKey: trpc.mail.get.queryKey({ id: threadId }),
+        });
+      }
+      console.log('party message', threadId, type);
     },
   });
 
