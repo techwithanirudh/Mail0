c7085059ddb24f709369bf3b065799e412f9d28c
Aj Wazzan <x_1337@outlook.com>
2025-05-11 18:46:12 -0700

    feat: enhance NotificationProvider to refetch labels and threads based on message timing

--- apps/mail/components/party.tsx
+++ apps/mail/components/party.tsx
@@ -3,13 +3,16 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useTRPC } from '@/providers/query-provider';
 import { usePartySocket } from 'partysocket/react';
 import { useThreads } from '@/hooks/use-threads';
+import { useLabels } from '@/hooks/use-labels';
 import { useSession } from '@/lib/auth-client';
 
 export const NotificationProvider = ({ headers }: { headers: Record<string, string> }) => {
   const trpc = useTRPC();
   const { data: session } = useSession();
+  const { refetch: refetchLabels } = useLabels();
   const queryClient = useQueryClient();
   const [{ refetch: refetchThreads }] = useThreads();
+  const [lastUpdated, setLastUpdated] = useState<number>(0);
   usePartySocket({
     party: 'durable-mailbox',
     room: session?.activeConnection?.id ? `${session.activeConnection.id}` : 'general',
@@ -24,12 +27,16 @@ export const NotificationProvider = ({ headers }: { headers: Record<string, stri
       console.warn('party message', message);
       const [threadId, type] = message.data.split(':');
       if (type === 'end') {
-        console.log('invalidating thread', threadId);
-        await queryClient.invalidateQueries({
-          queryKey: trpc.mail.get.queryKey({ id: threadId }),
-        });
-        await refetchThreads();
-        console.warn('refetched threads');
+        if (Date.now() - lastUpdated > 30000) {
+          console.log('invalidating thread', threadId);
+          await refetchLabels();
+          await queryClient.invalidateQueries({
+            queryKey: trpc.mail.get.queryKey({ id: threadId }),
+          });
+          await refetchThreads();
+          setLastUpdated(Date.now());
+          console.warn('refetched threads');
+        }
       }
       console.warn('party message', threadId, type);
     },
