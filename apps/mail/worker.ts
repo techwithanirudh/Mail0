18e6d9f9ac3b17a5aac26bb7a178d0c6c78577d2
BlankParticle <blankparticle@gmail.com>
2025-05-21 23:52:45 +0530

    chore: add sentry proxy

--- apps/mail/worker.ts
+++ apps/mail/worker.ts
@@ -19,8 +19,19 @@ const requestHandler = createRequestHandler(
   import.meta.env.MODE,
 );
 
+const sentryUrl = `https://o4509328786915328.ingest.us.sentry.io/api/4509328795303936/envelope/?sentry_version=7&sentry_key=03f6397c0eb458bf1e37c4776a31797c&sentry_client=sentry.javascript.react%2F9.19.0`;
+
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/monitoring')) {
+      const sentryRequest = new Request(sentryUrl, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+      });
+      return await fetch(sentryRequest);
+    }
     return requestHandler(request, {
       cloudflare: { env, ctx },
     });
