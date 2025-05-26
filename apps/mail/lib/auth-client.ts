7f9a1c3c103a1359e8a4a36a04dd2056d02c5ad2
Adam <x_1337@outlook.com>
2025-05-26 11:52:32 -0700

    refactor: remove unused customSessionClient from auth-client and auth-proxy (#1086)

--- apps/mail/lib/auth-client.ts
+++ apps/mail/lib/auth-client.ts
@@ -1,4 +1,4 @@
-import { customSessionClient, phoneNumberClient } from 'better-auth/client/plugins';
+import { phoneNumberClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 import type { Auth } from '@zero/server/auth';
 
@@ -7,7 +7,7 @@ export const authClient = createAuthClient({
   fetchOptions: {
     credentials: 'include',
   },
-  plugins: [customSessionClient<Auth>(), phoneNumberClient()],
+  plugins: [phoneNumberClient()],
 });
 
 export const { signIn, signUp, signOut, useSession, getSession, $fetch } = authClient;
